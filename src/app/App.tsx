debf55be0b0a58aca02fa201142cfb91b21d8508
denis <anywaymaybe1@gmail.com>
2024-12-09 23:14:40 +0400

    main: include react, prettier

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { TestComponent } from "shared/test-component";
+
+export const App = () => {
+    return (
+        <div>
+            <TestComponent />
+        </div>
+    );
+};
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,58 +3,61 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 
 module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  mode: "development",
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', "@babel/preset-typescript"]
-            }
-          },
-          {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true, // Отключает проверку типов в ts-loader
-            },
-          },
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
-    compress: true,
-    port: 3000,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-  ],
+    mode: "development",
+    resolve: {
+        alias: {
+            shared: path.resolve(__dirname, 'src/shared/'),
+        },
+        extensions: [".ts", ".js", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?tsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', "@babel/preset-typescript", ['@babel/preset-react']]
+                        }
+                    },
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true, // Отключает проверку типов в ts-loader
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "index.html",
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+    ],
 };
